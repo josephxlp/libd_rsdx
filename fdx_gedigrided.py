e148e2285cfc2ebe037d1c1f583c2eb29eea51b1
jzphlp <josephluntadilap@gmail.com>
2025-01-24 17:10:32 +0000

    improved functinallity to make it more adaptible to hfiles, and Xgrid

--- fdx_gedigrided.py
+++ fdx_gedigrided.py
@@ -1,56 +1,17 @@
 import os 
 from glob import glob
-from uvars import gedi_grid_dpath,base_files_dpathx
+from uvars import gedi_grid_dpath,base_files_dpathx,text, var1, var2
 from utils import  gwr_grid_downscaling
 import time 
-from pprint import pprint
-
-
-def print_context(text):
-    print(' ')
-    """
-    Prints the given text in a styled format, mimicking SAGA GIS console output.
-
-    Parameters:
-        text (str): The text to print.
-    """
-    # Border symbols
-    border_char = "=" * 60
-    padding_char = " " * 4
-
-    # Print formatted text
-    print(border_char)
-    print(f"{padding_char}ML - Process Log")
-    print(border_char)
-
-    for line in text.split("\n"):
-        print(f"{padding_char}{line}")
-
-    print(border_char)
-
-def select_base_file(tile_hfiles):
-    bfile = [i for i in tile_hfiles if 'edem_W84' in i]
-    pprint(bfile)
-    assert len(bfile) == 1, 'More than 1 file were selected'
-    return bfile[0]
-
-def select_filled_file(files, var,meth):
-    varpath = [i for i in files if var in i and meth in i]
-    pprint(varpath)
-    assert len(varpath) == 1, 'More than 1 file were selected'
-    return varpath[0]
-
+from utils import select_base_file,select_filled_file,print_context
 
 # remove all the unwanted files from the main function 
 # rh100:90:
 # elev:rh100
 tilenames = os.listdir(gedi_grid_dpath)
-#tilename = 'N09E105'
-var1 = 'elev' #'rh100'# 'elev'
-var2 = 'rh100'
+
 meth= 'riofill_si0.tif'#'riofill'
 
-text = "mean_2019108_2022019_002_03_EPSG4326_riofill_si0"
 
 X = 90#30 
 base_files_dpath = base_files_dpathx + str(X)
@@ -69,16 +30,16 @@ if __name__ == '__main__':
         rfpath1 = lfpath1.replace('.tif', f'_GWR_{str(X)}.sdat')
         rfpath1 = rfpath1.replace(text,'__').replace('elev_lowestmode','dtm')
         
-        #print_context(f"Processing {tilename} @{var1} \nSaving to {rfpath1}")
-        #gwr_grid_downscaling(xpath=hfpath, ypath=lfpath1, opath=rfpath1, oaux=False,epsg_code=4326)
+        print_context(f"Processing {tilename} @{var1} \nSaving to {rfpath1}")
+        gwr_grid_downscaling(xpath=hfpath, ypath=lfpath1, opath=rfpath1, oaux=False,epsg_code=4326)
 
         
         lfpath2 = select_filled_file(tile_lfiles, var2,meth)
         rfpath2 = lfpath2.replace('.tif', f'_GWR_{str(X)}.sdat')
         rfpath2 = rfpath2.replace(text,'__').replace('rh100','dsm')
         
-        #print_context(f"Processing {tilename} @{var2}\nSaving to {rfpath2}")
-        #gwr_grid_downscaling(xpath=hfpath, ypath=lfpath2, opath=rfpath2, oaux=False,epsg_code=4326)
+        print_context(f"Processing {tilename} @{var2}\nSaving to {rfpath2}")
+        gwr_grid_downscaling(xpath=hfpath, ypath=lfpath2, opath=rfpath2, oaux=False,epsg_code=4326)
         print('*'*50)
         
         #print(f"Processing @{var1} @{rfpath1}")
