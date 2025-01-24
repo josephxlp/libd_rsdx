993335599d09ef4b821146c7a3438345076536ff
jzphlp <josephluntadilap@gmail.com>
2025-01-24 11:27:56 +0000

    renaming the outfiles correctly

--- fdx_gedigrided.py
+++ fdx_gedigrided.py
@@ -50,7 +50,9 @@ var1 = 'elev' #'rh100'# 'elev'
 var2 = 'rh100'
 meth= 'riofill_si0.tif'#'riofill'
 
-X = 30 
+text = "mean_2019108_2022019_002_03_EPSG4326_riofill_si0"
+
+X = 90#30 
 base_files_dpath = base_files_dpathx + str(X)
 if __name__ == '__main__':
     ti = time.perf_counter()
@@ -59,22 +61,29 @@ if __name__ == '__main__':
 
         tile_lfiles = glob(f"{gedi_grid_dpath}/{tilename}/*.tif")
         tile_hfiles = glob(f"{base_files_dpath}/{tilename}/*.tif")
-
+        print('*'*50)
         print(len(tile_lfiles), len(tile_hfiles))
-
-        
 
         lfpath1 = select_filled_file(tile_lfiles, var1,meth)
         hfpath = select_base_file(tile_hfiles)
         rfpath1 = lfpath1.replace('.tif', f'_GWR_{str(X)}.sdat')
-        print_context(f"Processing {tilename} @{var1} \nSaving to {rfpath1}")
-        gwr_grid_downscaling(xpath=hfpath, ypath=lfpath1, opath=rfpath1, oaux=False,epsg_code=4326)
+        rfpath1 = rfpath1.replace(text,'__').replace('elev_lowestmode','dtm')
+        
+        #print_context(f"Processing {tilename} @{var1} \nSaving to {rfpath1}")
+        #gwr_grid_downscaling(xpath=hfpath, ypath=lfpath1, opath=rfpath1, oaux=False,epsg_code=4326)
 
         
         lfpath2 = select_filled_file(tile_lfiles, var2,meth)
         rfpath2 = lfpath2.replace('.tif', f'_GWR_{str(X)}.sdat')
-        print_context(f"Processing {tilename} @{var2}\nSaving to {rfpath2}")
-        gwr_grid_downscaling(xpath=hfpath, ypath=lfpath2, opath=rfpath2, oaux=False,epsg_code=4326)
+        rfpath2 = rfpath2.replace(text,'__').replace('rh100','dsm')
+        
+        #print_context(f"Processing {tilename} @{var2}\nSaving to {rfpath2}")
+        #gwr_grid_downscaling(xpath=hfpath, ypath=lfpath2, opath=rfpath2, oaux=False,epsg_code=4326)
+        print('*'*50)
+        
+        #print(f"Processing @{var1} @{rfpath1}")
+        #print(f"Processing @{var2} @{rfpath2}")
+        print(' ')
 
     
     tf = time.perf_counter() - ti 
